f8980af2705a79bb4ccd79095206ff6e7bb9f8a4
Samisam2005 <samanthajulissagutierrez@gmail.com>
2024-10-06 14:02:33 -0600

    primer cambio confirmado

---
+++ ControlAsistencia/Program.cs
@@ -0,0 +1,42 @@
+﻿using System; 
+
+namespace ControlAsistencia
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Universidad.Estudiante estudiante1 = new Universidad.Estudiante
+            {
+                Nombre = "Marta",
+                SesionesTotales = 40,
+                SesionesAsistidas = 40,
+            };
+
+            Universidad.Estudiante estudiante2 = new Universidad.Estudiante
+            {
+                Nombre = "Belkin",
+                SesionesTotales = 40,
+                SesionesAsistidas = 35,
+            };
+
+            Universidad.Estudiante estudiante3 = new Universidad.Estudiante
+            {
+                Nombre = "Angello",
+                SesionesTotales = 40,
+                SesionesAsistidas = 17,
+            };
+
+            Universidad.Estudiante[] estudiantes = new Universidad.Estudiante[3];
+            estudiantes[0] = estudiante1;
+            estudiantes[1] = estudiante2;
+            estudiantes[2] = estudiante3;
+
+            foreach (Universidad.Estudiante estudiante in estudiantes)
+            {
+                estudiante.MostrarInformacion();
+                Console.WriteLine("---------------------------------------------------------------------");
+            }
+        }
+    }
+}
---
+++ ControlAsistencia/Universidad.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ControlAsistencia
+{
+    public class Universidad
+    {
+        public class Estudiante
+        {
+            public string Nombre { get; set; }
+            public int SesionesTotales { get; set; }
+            public int SesionesAsistidas { get; set; }
+
+            public class Asistencia
+            {
+                public static double CalcularPorcentajeAsistencia(int sesionesTotales, int sesionesAsistidas)
+                {
+                    if (sesionesTotales == 0) return 0;
+                    return (double)sesionesAsistidas / sesionesTotales * 100;
+                }
+
+                public static string VerificarAsistencia(int sesionesTotales, int sesionesAsistidas)
+                {
+                    double porcentajeAsistencia = CalcularPorcentajeAsistencia(sesionesTotales, sesionesAsistidas);
+                    if (porcentajeAsistencia >= 75)
+                    {
+                        return $"El estudiante cumple con el mínimo de asistencia: {porcentajeAsistencia}%";
+                    }
+                    else
+                    {
+                        return $"El estudiante NO cumple con el mínimo de asistencia: {porcentajeAsistencia}%";
+                    }
+                }
+            }
+        }
+    }
+
+    public static class EstudianteExtencion
+    {
+        public static void MostrarInformacion(this ControlAsistencia.Universidad.Estudiante estudiante)
+        {
+            double porcentajeAsistencia = ControlAsistencia.Universidad.Estudiante.Asistencia.CalcularPorcentajeAsistencia(estudiante.SesionesTotales, estudiante.SesionesAsistidas);
+            string cumplimiento = ControlAsistencia.Universidad.Estudiante.Asistencia.VerificarAsistencia(estudiante.SesionesTotales, estudiante.SesionesAsistidas);
+
+            Console.WriteLine($"Nombre: {estudiante.Nombre}");
+            Console.WriteLine($"Sesiones Totales: {estudiante.SesionesTotales}");
+            Console.WriteLine($"Sesiones Asistidas: {estudiante.SesionesAsistidas}");
+            Console.WriteLine($"Porcentaje de Asistencia: {porcentajeAsistencia}%");
+            Console.WriteLine(cumplimiento);
+        }
+    }
+}
+
